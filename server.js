cedbecfdc87a84f66031730559b32dbe4ab99e6d
Angel Ancheta <angelancheta98@yahoo.com>
2019-06-15 13:09:07 -0700

    error fix

--- server.js
+++ server.js
@@ -176,6 +176,6 @@ app.get("/clearall", function(req, res) {
 });
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen( process.env.PORT || 3000, function() {
   console.log("App running on port 3000!");
 });
